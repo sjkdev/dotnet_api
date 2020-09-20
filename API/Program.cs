c2bdcf689fce0dab5d130f60f2aac07c940f0bce
seanjamesking <helloseanking@yahoo.com>
2020-09-20 19:41:01 +0100

    init repo

---
+++ API/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    {
+                        var context = services.GetRequiredService<DataContext>();
+                        context.Database.Migrate();
+                    }
+                }
+                // catch (System.Exception)
+                // {
+                    
+                //     throw;
+                // }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration");
+                }
+            }
+
+            host.Run();
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
